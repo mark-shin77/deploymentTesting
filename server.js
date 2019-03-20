62d33df9bbc31ffe69ce0b0ba09cc97a50f3fc85
Mark <contact.mshin@gmail.com>
2019-03-19 19:51:01 -0600

    test 7

--- server.js
+++ server.js
@@ -31,14 +31,12 @@ app.use("/api", apiRoutes);
 
 // Serve up static assets (usually on heroku)
 // 
-
-
-db.sequelize.sync({})
-
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+db.sequelize.sync({force: false}).then(function(){
     app.listen(PORT, function(){
         console.log(`APP listening on PORT ${PORT}`)
- });
+    });
+})
